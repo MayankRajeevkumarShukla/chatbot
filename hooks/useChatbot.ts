e47db6361cf1740fba544f71f00ab2eb207e421c
Sahil Gulihar <sahilgullihar@gmail.com>
2025-05-27 00:00:24 +0530

    model change

--- hooks/useChatbot.ts
+++ hooks/useChatbot.ts
@@ -14,7 +14,7 @@ if (!API_KEY) {
 
 const genAI = new GoogleGenerativeAI(API_KEY);
 const model = genAI.getGenerativeModel({
-  model: "gemini-1.5-flash-latest",
+  model: "gemini-2.5-flash-preview-04-17",
 });
 
 
